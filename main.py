f3be5571668267b53c93d33edb27543e1418e60d
HAEIN PARK <sunman365@naver.com>
2020-11-28 08:32:39 +0000

    download results as csv file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request, redirect
+
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
 
+"""
+Indeed("https://kr.indeed.com") 에서 직업 정보를 받아와 웹페이지로 보여준후 csv 파일로 다운로드를 할 수 있다. 
+"""
 app = Flask("SuperScrapper")
 
-db = {} #report나 home이 재실행되어도 초기화되지않도록
+db = {} 
+#fake_DB (directory를 DB처럼 사용)
+#report나 home이 재실행되어도 초기화되지않도록
 
 @app.route("/")
 def home():
@@ -22,12 +28,75 @@ def report():
       db[word] = jobs
   else :
     return redirect("/")
-  return render_template(
-    "report.html",
-    searchingBy = word, 
-    results_number = len(jobs),
-    jobs = jobs
-  )
+    
+  return render_template("report.html", searchingBy = word, results_number = len(jobs), jobs = jobs)
 
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    return send_file("jobs.csv")
 
+  except:
+    return redirect("/")
 app.run(host = "0.0.0.0")
+
+"""
+from flask import Flask, render_template, request,  redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
+
+app = Flask("BEAN MADE THIS")
+
+
+db = {}
+
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+#it's a same thing like instagram assessing ways 
+#@ is decolator, so it just find a def
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  if word:
+    word = word.lower()
+    existingJobs = db.get(word)
+    if existingJobs:
+      jobs = existingJobs
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
+  else:
+    return redirect("/")
+
+  return render_template("report1.html", SearchingBy = word, resultsNumber= len(jobs), jobs =jobs)
+  # it's a mechanism about the html&python
+
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+
+  except:
+    return redirect("/")
+app.run(host = "0.0.0.0")
+# host = "0.0.0.0" this is a tool that it can be shown to us our page on repl.it
+"""
--- scrapper.py
+++ scrapper.py
@@ -1,50 +1,64 @@
 import requests
 from bs4 import BeautifulSoup
+from exporter import save_to_file
+
+LIMIT = 50
 
 
-def get_last_page(URL):
-  request = requests.get(URL)
-  soup = BeautifulSoup(request.text,"html.parser")
-  pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
-  last_pages = pages[-2].find("span").string
-  return int(last_pages)
+def get_last_pages(URL) : 
+  try:
+    request = requests.get(URL)
+    soup = BeautifulSoup(request.text, ("html.parser"))
+
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all("span", {"class" : "pn"})
+    pages = []
+
+    for link in links[:-1]:
+      pages.append(int(link.string))
+
+    max_page = pages[-1]
+  except:
+    return None
+  return int(max_page)
+
+
+def extract_job(job_soup):
+  title = job_soup.find("h2",{"class" : "title"}).find("a")["title"]
+  company = job_soup.find("span", {"class" : "company"})
+  location = job_soup.find("div", {"class" : "recJobLoc"})["data-rc-loc"]
+
+  company_anchor = company.find("a")
+  if company_anchor is None:
+    company = company.string
+  else:
+    company = company_anchor.string
+  company = company.strip()
+
+  job_id = job_soup["data-jk"]
+
+  return {'title':title, 'company':company, 'location':location, 'link': f"https://kr.indeed.com/viewjob?jk={job_id}"}
 
 
 def extract_jobs(last_page, URL):
   jobs = []
-  for page in range(last_page):
-    print(f"Scrapping SO: Page: {page}")
-    request = requests.get(f"{URL}&pg={page+1}")
+  
+  for page in range(last_page): 
+    print(f"Scrapping Page: {page}")
+    request = requests.get(f"{URL}&start={0*LIMIT}")
     soup = BeautifulSoup(request.text,"html.parser")
-    results = soup.find_all("div",{"class":"-job"})
+    results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
     for result in results:
       job = extract_job(result)
       jobs.append(job)
-  
   return jobs
 
-
-def extract_job(html):
-  title = html.find("h2",{"class":"mb4"}).find("a")["title"]
-  company, location = html.find("h3").find_all("span", recursive=False) # unpacking
-  #recursive는 span안에 포함된 span을 가져오지 않도록
-
-  company = company.get_text(strip=True)
-  location = location.get_text(strip=True)
-
-  job_id = html['data-jobid']
-
-  return {
-    'title': title, 
-    'company': company, 
-    'location': location, 
-    'apply_link': f"https://stackoverflow.com/jobs/{job_id}"
-    }
-
-
 def get_jobs(word):
-  
-  URL = f"https://stackoverflow.com/jobs?q={word}"
-  last_page = get_last_page(URL)
-  jobs = extract_jobs(last_page, URL)
+  URL = f"https://kr.indeed.com/취업?q={word}&limit={LIMIT}"
+  last_page = get_last_pages(URL)
+  if last_page == None:
+    return []
+  else:
+    jobs = extract_jobs(last_page, URL)
+    save_to_file(jobs)
   return jobs
